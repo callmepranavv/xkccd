e4e16dc1450a7b88531d333bea406ee76a813c54
ykram2019 <44055979+ykram2019@users.noreply.github.com>
2021-07-12 21:44:26 +0545

    Update unsubscribe.php

--- unsubscribe.php
+++ unsubscribe.php
@@ -1,3 +1,5 @@
+
+
 <?php
 require 'configure.php';
 $email=$_GET['email'];
@@ -6,7 +8,17 @@ $email=$_GET['email'];
 $sql="DELETE FROM `users` WHERE email ='$email'";
 if ($con->query($sql) === TRUE) 
                 {
-                    echo "You are unsubscribed now";
+                    echo "<html>
+<head>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="message"><h2> You are unsubscribed now</h2> </div>
+</body>
+</html>
+                   
+                    ";
                     
                   } else {
                     echo "Error: " . $sql . "<br>" . $con->error;
