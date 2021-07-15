1645112f3950a523b92065d46739dd7a652e364f
ykram2019 <44055979+ykram2019@users.noreply.github.com>
2021-07-15 19:52:27 +0545

    Update unsubscribe.php

--- unsubscribe.php
+++ unsubscribe.php
@@ -5,7 +5,7 @@ require 'configure.php';
 $id=$_GET['id'];
 
 
-$sql="DELETE FROM `users` WHERE id ='$id'";
+$sql="DELETE FROM `users` WHERE Id ='$Id'";
 if ($con->query($sql) === TRUE) 
                 {
                     echo "You are Undubdcribed now";
